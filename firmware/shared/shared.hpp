397ca3e7f4aea64d5b78e25c642d565b763bb5a3
Lucas Mösch <lucasmoesch@live.de>
2025-08-29 23:20:57 +0200

    Added nanopb string decode callback

--- firmware/shared/shared.hpp
+++ firmware/shared/shared.hpp
@@ -2,6 +2,8 @@
 #define SHARED_HPP
 
 #include <pb_encode.h>
+#include <pb_decode.h>
+
 namespace shared
 {
     uint8_t trackerId = 1;
@@ -29,6 +31,19 @@ namespace shared
             return false;
 
         return pb_encode_string(stream, (uint8_t *)str, strlen(str));
+    }
+
+    bool decode_string(pb_istream_t *stream, const pb_field_t *field, void **arg)
+    {
+        const char *str = (const char *)(*arg);
+
+        if (stream->bytes_left > sizeof(str) - 1)
+            return false;
+
+        if (!pb_read(stream, (uint8_t *)str, stream->bytes_left))
+            return false;
+
+        return true;
     }
 }
 
